0f48cd82bc4e19a93e46e108c8bfc71917932498
Maria Antonia <mdeazevedomaia@gmail.com>
2023-08-23 18:03:49 -0300

    Unidade 03

--- conta.py
+++ conta.py
@@ -9,3 +9,21 @@ class Conta:
         self.titular = titular
         self.saldo = saldo
         self.limite = limite
+
+    # Funções específicas -> método //  utiliza parametro (self) referência do obj
+    def extrato(self):
+        print("Saldo de {} do titular {}".format(self.saldo, self.titular))
+
+    # Parâmetro valor utilizado para podermos definir o valor a mais ou menos na conta
+    def deposita(self, valor):
+        self.saldo += valor
+
+    def saca(self, valor):
+        self.saldo -= valor
+
+
+
+    # Há a possibilidade de criar variaveis identicas, perdendo a referência do objeto, 
+    # para isso o python possui a função `garbage collector´
+
+    # None - pode ser utilizado caso tenham mais referências (xerox), que não quer mais utilizar
